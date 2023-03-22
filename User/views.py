b7727846e5f4e2ecf855c851e95f2d7ef5843be3
zouzx21 <zouzx21@mails.tsinghua.edu.cn>
2023-03-22 17:12:12 +0800

    deploy_test_001

--- User/views.py
+++ User/views.py
@@ -1,7 +1,8 @@
 import json
 from django.http import HttpRequest, HttpResponse
 
-from User.models import Entity,Department, User, AssetClass, Asset, PendingRequests
+from User.models import Entity,Department, User
+from Asset.models import AssetClass, Asset, PendingRequests
 from utils.utils_request import BAD_METHOD, request_failed, request_success, return_field
 from utils.utils_require import MAX_CHAR_LENGTH, CheckRequire, require
 from utils.utils_time import get_timestamp
