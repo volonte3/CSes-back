db0f2a1e177df12a592592e56ca5f2f956755a1c
邹作休 <2021010709@secoder.net>
2023-03-29 15:08:31 +0000

    [001]将deploy_test合并到dev并彻底删掉前者

--- User/tests.py
+++ User/tests.py
@@ -1,9 +1,90 @@
 from django.test import TestCase
-from User.models import Entity,Department, User
-from Asset.models import AssetClass, Asset, PendingRequests
+from User.models import *
+from django.test import Client as DefaultClient
+from User.views import *
+import json
+
+
+class Client(DefaultClient):
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+
+    def _add_cookie(self, kw):
+        if "SessionID" in self.cookies:
+            if "data" not in kw:
+                kw["data"] = {}
+            kw["data"]["SessionID"] = self.cookies["SessionID"].value
+        return kw
+
+    def post(self, *args, **kw):
+        return super(Client, self).post(*args, **self._add_cookie(kw))
+
+    def get(self, *args, **kw):
+        return super(Client, self).get(*args, **self._add_cookie(kw))
+
+
 class UserTests(TestCase):
     def setUp(self):
-        pass
-    def test_assert_test(self):
-        self.assertEqual(1, 1)
-# Create your tests here.
+        self.e1 = Entity.objects.create(name = 'CS_Company')
+        self.d1 = Department.objects.create(name = 'depart1',entity = self.e1)
+        self.u1 = User.objects.create(
+            name = 'chusheng_1',password = 'yiqunchusheng',
+            entity = self.e1,department= self.d1,
+            entity_super = 0,system_super = 1
+        )
+        self.u2 = User.objects.create(
+            name="chusheng_2", password="yiqunchusheng",
+            entity = self.e1,department= self.d1,
+            entity_super = 1,system_super = 0
+        )
+        self.u3 = User.objects.create(
+            name="chusheng_3", password="yiqunchusheng",
+            entity = self.e1,department= self.d1,
+            entity_super = 0,system_super = 0
+        )
+        SessionPool.objects.create(user = self.u1)
+    
+    def test_login1(self):
+        c = Client()
+        c.cookies["SessionID"] = "0"
+        resp = c.post(
+            "/User/login",
+            data={"UserName": self.u1.name +'hahaha', "Password": self.u1.password},
+            content_type="application/json",
+        )
+        self.assertEqual(resp.json()["code"], 2)
+
+    def test_login2(self):
+        c = Client()
+        c.cookies["SessionID"] = "0"
+        resp = c.post(
+            "/User/login",
+            data={"UserName": self.u1.name, "Password": self.u1.password + 'hahaha'},
+            content_type="application/json",
+        )
+        self.assertEqual(resp.json()["code"], 3)
+        
+    def test_login3(self):
+        c = Client()
+        c.cookies["SessionID"] = "0"
+        resp = c.post(
+            "/User/login",
+            data={"UserName": self.u1.name, "Password": self.u1.password},
+            content_type="application/json",
+        )
+        self.assertEqual(resp.json()["code"], 0)
+    
+    def test_logout(self):
+        c = Client()
+        c.cookies["SessionID"] = "0"
+        c.post(
+            "/User/login",
+            data={"UserName": self.u1.name, "Password": self.u1.password},
+            content_type="application/json",
+        )
+        resp = c.post(
+            "/User/logout",
+            content_type="application/json",
+        )
+        self.assertEqual(resp.json()["code"], 0)
+
