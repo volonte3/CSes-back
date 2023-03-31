e105d9df838766fcb155e22295de250f2012f68e
范孙奇 <2020012497@secoder.net>
2023-03-31 22:28:42 +0800

    [001]增加了user_info函数的路由

--- User/urls.py
+++ User/urls.py
@@ -3,5 +3,6 @@ import User.views as views
 
 urlpatterns = [
     path('/login', views.login),
-    path('/logout', views.logout)
+    path('/logout', views.logout),
+    path('info',views.user_info)
 ]
