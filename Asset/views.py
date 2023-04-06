9e8c6b5224e3f6140e72d1fe13f9d5e124643f5a
范孙奇 <2020012497@secoder.net>
2023-04-06 14:15:34 +0800

    “[002]进一步完善资产分类树

--- Asset/views.py
+++ Asset/views.py
@@ -1,78 +1,45 @@
-from Asset.models import Asset, AssetClass
-from User.models import SessionPool
-from rest_framework.request import Request
-from utils.sessions import get_session_id
-from utils.utils_other import *
-from utils.utils_request import BAD_METHOD, request_failed, request_success, return_field
-import pytz
-import datetime as dt
-from CS_Company_backend.settings import TIME_ZONE
-from utils.utils_asset import parse_children, give_subtree_recursive
+from Asset.models import AssetClass
+from User.models import User
+from utils.utils_request import request_success
+from utils.utils_asset import * 
+from utils.config import *
 
-def add_asset_class():
+def _add_asset_class(usr:User, data):
+
+    # TODO: 判断是否存在同名的资产分类
+
+    AssetClass.objects.create(
+        deparment = usr.department, name = data["asset_class_name"], \
+        parent = get_asset_class(data["parent_node_value"]), children = "", \
+        property = 1 # TODO:先暂时定成1
+    )
+
+
+
+def add_asset_class(req: Request):
+    # 先解析出数据, 
     pass
     # 增加一个资产类别类
 
 
 
+def _give_tree(usr, data=None):
+    # 根据这个usr的department_id来给出这个department的资产分类树
+    treeData = []
 
+    # 找到这个department的根节点
+    rootNode = AssetClass.objects.filter(department = usr.department, property = 0).first()
+    # 遍历这个根节点的孩子节点
+    children_list = parse_children(rootNode.children)
+    for child_id in children_list:
+        treeData.append(give_subtree_recursive(child_id, usr.department.id))
 
+    return request_success({"treeData":treeData})
 
 def give_tree(req: Request):
-
-    # TODO: 检验这个Request请求的正确性,下面先写正确的一支
-
-    if req.method == 'POST':
-        session_id = get_session_id(req)
-    
-    # 下面是 session_id to user的过程
-    sessionRecord =SessionPool.objects.filter(sessionId=session_id).first()
-    if sessionRecord:
-        if sessionRecord.expireAt < dt.datetime.now(pytz.timezone(TIME_ZONE)):
-            SessionPool.objects.filter(sessionId=session_id).delete()
-            return request_failed(2, "session id expire")
-        else:
-            usr = sessionRecord.user
-
-            # 根据这个usr的department_id来给出这个department的资产分类树
-            treeData = []
-
-            # 找到这个department的根节点
-            rootNode = AssetClass.objects.filter(department = usr.department, property = 0).first()
-            # 遍历这个根节点的孩子节点
-            children_list = parse_children(rootNode.children)
-            for child_id in children_list:
-                treeData.append(give_subtree_recursive(child_id, usr.department.id))
-  
-            return request_success({"treeData":treeData})
-    else:
-        return request_failed(1, "session id doesn't exist")
+    return session_id_to_user(req=req, function=_give_tree, authority_level=ONLY_ASSET_ADMIN)
 
 
-
-
-    # 给前端返回一个树, 采用json套json的格式, 参考https://ant.design/components/tree-select-cn格式
-    # 格式样例：
-    # const treeData = [
-    #     {
-    #         title: 'Node1',
-    #         value: '0-0',
-    #         children: [
-    #         {
-    #             title: 'Child Node1',
-    #             value: '0-0-1',
-    #         },
-    #         {
-    #             title: 'Child Node2',
-    #             value: '0-0-2',
-    #         },
-    #         ],
-    #     },
-    #     {
-    #         title: 'Node2',
-    #         value: '0-1',
-    #     },
-    # ];
     
 
 
--- utils/utils_asset.py
+++ utils/utils_asset.py
@@ -1,8 +1,54 @@
-from Asset.models import AssetClass, Asset, Department
+from Asset.models import AssetClass
+from User.models import Department
+from User.models import SessionPool
+from rest_framework.request import Request
+from utils.sessions import get_session_id
+from utils.utils_other import *
+from utils.utils_request import BAD_METHOD, request_failed
+import pytz
+import datetime as dt
+from CS_Company_backend.settings import TIME_ZONE
 
 # 可以多写一个鉴定用户是不是资产管理、有没有权限修改的函数
+def check_authority(authority_level):
+    # TODO 
+    # 可以用宏定义,更加清晰一些
+    return None
 
-def get_asset(asset_class_id):
+def session_id_to_user(req, function, authority_level, data=None):
+
+    if req.method == 'POST':
+        session_id = get_session_id(req)
+    
+        # 下面是 session_id to user的过程
+        sessionRecord =SessionPool.objects.filter(sessionId=session_id).first()
+        if sessionRecord:
+            if sessionRecord.expireAt < dt.datetime.now(pytz.timezone(TIME_ZONE)):
+                SessionPool.objects.filter(sessionId=session_id).delete()
+                return request_failed(2, "session id expire")
+            else:
+                usr = sessionRecord.user
+
+                # TODO
+                check_result = check_authority(authority_level)
+                if check_result != None:
+                    return check_result
+                
+                # TODO:从req中解析出数据
+                # 使用return field
+
+                return function(usr, data)           
+        else:
+            return request_failed(1, "session id do not exist")
+    else:
+        return BAD_METHOD
+
+def get_department(department_id):
+    department = Department.objects.filter(id=department_id)
+    assert department != None, "Department NOT Found!"
+    return department
+
+def get_asset_class(asset_class_id):
     asset_class = AssetClass.objects.filter(id=asset_class_id).first()
     assert asset_class != None, "Asset Class NOT Found!"
     return asset_class
@@ -16,7 +62,7 @@ def give_subtree_recursive(asset_class_id, department_id):
     nodeData = {}
 
     # 把这个asset_class的title与value填写到nodeData中, value直接写asset_class_id
-    asset_class = get_asset(asset_class_id)
+    asset_class = get_asset_class(asset_class_id)
     assert asset_class.department.id == department_id, "department id is wrong!"
 
     nodeData['title'] = asset_class.name
